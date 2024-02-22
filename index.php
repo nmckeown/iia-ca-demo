681fba688c658cafb9e718ce9b83d1a1aaab7444
nmckeown <noelmckeown@gmail.com>
2024-02-22 22:09:31 +0000

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
  <head>
  </head>
  <body>
-   IIA CA1 March 2024
+   IIA CA1 March 2024 Server1
    <h1>IE_GOV WEB Server</h1>
    <p>Noel McKeown - x00218705</p>
+   <?php echo gethostname(); ?>
  </body>
 </html>
